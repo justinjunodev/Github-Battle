76b60d7d66c52c05b2f9f02f923ff2ec2eb550cc
Justin Juno <justin@edtechdeveloper.com>
2019-06-28 16:58:46 -0500

    create popular navbar component and add css

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./reset.css";
+import "./style.css";
+import Popular from "./components/Popular";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
+      <div className="container">
+        <Popular />
       </div>
     );
   }
